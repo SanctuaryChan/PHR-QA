706e2ada6279ce4392bbc1b5b1ee67449df5fe7b
Chen <chen@Chens-MacBook-Pro.local>
2026-02-10 22:36:44 +0800

    fix-输出评估细节

--- script/phase1.sh
+++ script/phase1.sh
@@ -1,6 +1,6 @@
-  python src/run.py phase1 \
-    --data_dir /data/GNN-RAG/datasets/webqsp \
-    --split dev \
-    --reader hf \
-    --model_path /data/GNN-RAG/models/Llama-2-7b-chat-hf \
-    --progress
+python src/run.py phase1 \
+  --data_dir /data/GNN-RAG/datasets/webqsp \
+  --split dev \
+  --reader hf \
+  --model_path /data/GNN-RAG/models/Llama-2-7b-chat-hf \
+  --save_prompt
